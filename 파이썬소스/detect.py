992559a2bf9cd548093d4b95af359164eb7cfde4
hohoprince <hoot0512@gmail.com>
2021-05-06 21:07:22 +0900

    detect.py 수정, decode.java 추가

--- 파이썬소스/detect.py
+++ 파이썬소스/detect.py
@@ -87,7 +87,9 @@ def detect(save_img=False):
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
+
+            ## 이미지 저장 경로
+            save_path = '../detect/' + p.name # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
@@ -95,12 +97,21 @@ def detect(save_img=False):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
+                t_result = []
+
                 # Print results
                 with open('result.txt', 'w') as f:
                     for c in det[:, -1].unique():
                         n = (det[:, -1] == c).sum()  # detections per class
+                        t_result.append(f"{names[int(c)]}:{n}")
                         s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-                        f.write(f'{names[int(c)]}:{n}\n')
+                        f.write(f'{names[int(c)]:{n}}\n')
+                print(t_result)
+
+                # 검출 결과 텍스트파일 저장
+                with open('../detect/' + p.stem + '.txt','w') as f:
+                    for result in t_result:
+                        f.write(result+ '\n')
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
