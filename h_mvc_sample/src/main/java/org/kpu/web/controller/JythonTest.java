fc16f4ca8bf547710cde00f75adb2a86e043710a
HMC <1170313@naver.com>
2021-05-10 22:59:38 +0900

    ProcessBuilder를 이용한 외부파일실행

--- h_mvc_sample/src/main/java/org/kpu/web/controller/JythonTest.java
+++ h_mvc_sample/src/main/java/org/kpu/web/controller/JythonTest.java
@@ -1,10 +1,15 @@
 package org.kpu.web.controller;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
@@ -30,13 +35,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/receive")
 public class JythonTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(JythonTest.class);
 	
-	@PostMapping(value = "/test")
-	public String getTest() throws IOException, InterruptedException {
-//		String[] arguments = {"detect.py", "--source", "476.jpg"};
+	@RequestMapping(value = "/receiveImage", method = RequestMethod.GET)
+	public String getTest(HttpServletRequest request) throws IOException, InterruptedException {
+		Runtime rt = Runtime.getRuntime();
+		Process pc = null;
+		
+		//		String[] arguments = {"detect.py", "--source", "476.jpg"};
 //		Properties p = new Properties();
 //		p.setProperty("python.path", "/home/ubuntu/yolov5");
 //		
@@ -60,12 +69,40 @@ public class JythonTest {
 //	        process.destroy();
 //	        return "json";
 //	    }
+//		String cmd = "python3 /var/lib/yolov5/detect.py --source /var/lib/tomcat9/webapps/upload/210-1.jpg";
+//		String[] command = {""+cmd+""};
 		
-        Process process = Runtime.getRuntime().exec("python3 /home/ubuntu/yolov5/detect.py --source /var/lib/tomcat9/webapps/upload/202105061557.jpg");
-        process.waitFor();
-        process.destroy();
+		
+//		try {
+//			pc = rt.exec("python3 /var/lib/yolov5/detect.py --source /var/lib/tomcat9/webapps/upload/210-1.jpg");
+//		}
+//		catch (IOException e){
+//			e.printStackTrace();
+//		}
+//		finally {
+//	        pc.waitFor();
+//	        pc.destroy();
+//		}
 
 		
+		List<String> cmdList = new ArrayList<String>(); // 운영체제 구분 (window, window 가 아니면 무조건 linux 로 판단) 
+		cmdList.add("sh");
+		cmdList.add("shell");
+		cmdList.add("--source"); 
+		cmdList.add("/var/lib/tomcat9/webapps/upload/210-1.jpg");
+//
+//		String[] array = cmdList.toArray(new String[cmdList.size()]);
+//
+//		pc = rt.exec(array);
+//	    pc.waitFor();
+//	    pc.destroy();
+		
+		
+		ProcessBuilder pb = new ProcessBuilder(cmdList);
+		pb.directory(new File("/var/lib/yolov5"));
+		pc = pb.start();
+		
+
 		return "success";
 	}
 }
